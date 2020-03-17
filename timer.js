d08cd2b42965a3fc9d2c1af5f5c7e88316481552
JC <james.calingo@gmail.com>
2020-03-17 19:26:56 -0400

    added the announcement date

--- timer.js
+++ timer.js
@@ -1,5 +1,3 @@
+const announcement = moment("20180918", "YYYYMMDD").fromNow();
 const release = moment("20200320", "YYYYMMDD").fromNow();
-$(".main").html(`Animal Crossing: New Horizons comes out ${release}!`)
-
-
-const time=moment().endOf('day').fromNow();
+$(".main").html(`Animal Crossing: New Horizons was announced over ${announcement} and comes out ${release}!`)
