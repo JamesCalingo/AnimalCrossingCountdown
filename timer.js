0840d8f41ecb2eb09f1f228800702e15322ec958
JC <james.calingo@gmail.com>
2020-03-19 21:09:12 -0400

    switched hours to minutes

--- timer.js
+++ timer.js
@@ -1,9 +1,9 @@
 const currentTime = moment()
 const announcement = moment("20180918", "YYYYMMDD")
 const announcementTime = currentTime.diff(announcement, "months")
-const release = moment("20200320", "YYYYMMDD").diff(currentTime, "hours");
+const release = moment("20200320", "YYYYMMDD").diff(currentTime, "minutes");
 const countdown = moment().endOf('day').fromNow();
 $(".main").html(`<h2>Dawn of</h2>
 <h1>The Final Day</h1>
 <p>(Until Animal Crossing: New Horizons)</p>
-<h3>-${release} hours Remain-</h3>`);
+<h3>-${release} minutes Remain-</h3>`);
