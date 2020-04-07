741d7b93316a78100e88d433c561c1a55c2fe638
JC <james.calingo@gmail.com>
2020-04-07 15:19:58 -0400

    made some changes

--- timer.js
+++ timer.js
@@ -1,10 +1,7 @@
 const currentTime = moment()
 const announcement = moment("20180918", "YYYYMMDD")
 const announcementTime = currentTime.diff(announcement, "months")
-const release = moment("20200320", "YYYYMMDD").diff(currentTime, "minutes");
-const seconds = moment("20200320", "YYYYMMDD").diff(currentTime, "seconds");
-$(".main").html(`<h2>Dawn of</h2>
-<h1>The Final Day</h1>
-<p>(Until Animal Crossing: New Horizons)</p>
-<h3>-${release} minutes Remain-</h3>
-(that's ${seconds} seconds)`);
+const release = moment("20200320", "YYYYMMDD").diff(currentTime, "days") * -1;
+const mins = moment("20200320", "YYYYMMDD").diff(currentTime, "minutes") * -1;
+$(".main").html(`<h1>Animal Crossing has consumed my life for about ${release} days now</h1><br>
+<p>(That's ${mins} minutes BTW since March 20, 2020 at Midnight Eastern)`);
