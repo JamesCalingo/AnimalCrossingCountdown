37eab45b84f5a9cdbcd1e2633ac7f795b50804e6
JC <james.calingo@gmail.com>
2020-06-22 15:00:08 -0400

    updated arrays

--- test.java
+++ test.java
@@ -5,9 +5,3 @@ public class Touchdown {
     System.out.println("Touchdown like " + actions[0] + " in a " + places[1] + "--we are parked and proud!");
   }
 }
-
-public class GoHome{
-  public static void main(String[] args) {
-    System.out.println("test");
-  }
-}
