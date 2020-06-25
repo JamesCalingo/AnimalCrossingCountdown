5b7cacf275560b352f280f322f5b35f269b629e2
JC <james.calingo@gmail.com>
2020-06-25 10:37:16 -0400

    added link to madlibs

--- madlibs.js
+++ madlibs.js
@@ -1,11 +1,12 @@
-const actions = ["eating spicy food", "flamenco dancing", "collecting stamps", "surfing the web", "fixing things", "window shopping", "woodworking", "collecting figurines", "reading fortunes", "making playlists", "watching movies", "learning languages", "taking long naps", "watching trash TV", "practicing kung fu", "growing vegetables", "eating free pizza", "making mosaics"];
+const actions = ["eating spicy food", "flamenco dancing", "collecting stamps", "surfing the web", "fixing things", "window shopping", "woodworking", "collecting figurines", "reading fortunes", "making playlists", "watching movies", "learning languages", "taking long naps", "watching trash TV", "practicing kung fu", "growing vegetables", "eating free pizza", "making mosaics", "bungee jumping"];
 
-const places = [" toothbrush", " glasses case", " pocketbook", " microphone", " photo album", " microscope", " hand mirror", " change jar", " fitted sheet", " leg warmer", " blue crayon", " calculator", " saltshaker", "n electric razor", " jewelry box", " ballpoint pen", "n inkjet printer", " jackhammer"];
+const places = [" toothbrush", " glasses case", " pocketbook", " microphone", " photo album", " microscope", " hand mirror", " change jar", " fitted sheet", " leg warmer", " blue crayon", " calculator", " saltshaker", "n electric razor", " jewelry box", " ballpoint pen", "n inkjet printer", " jackhammer", 
+" beach towel", " pencil case"];
 // The space/n before the item is to make it appear properly and grammatically correct in the sentence. This is the only place where it is necessary.
 
-const desserts = ["almond jelly", "gingerbread", "shaved ice", "cotton candy", "apple cobbler", "fried dough", "Bavarian cream", "rainbow sherbet", "key-lime pie", "apple cake", "peach cobbler", "tarte tatin", "crème brûlée", "coffee cake", "pumpkin pie", "coconut cream pie", "peanut brittle", "cheesecake", "spice cake", "fried ice cream", "bread pudding", "figgy pudding", "vanilla gelato", "banana bread", "apple strudel", "sponge cake"];
+const desserts = ["almond jelly", "gingerbread", "shaved ice", "cotton candy", "apple cobbler", "fried dough", "Bavarian cream", "rainbow sherbet", "key-lime pie", "apple cake", "peach cobbler", "tarte tatin", "crème brûlée", "coffee cake", "pumpkin pie", "coconut cream pie", "peanut brittle", "cheesecake", "spice cake", "fried ice cream", "bread pudding", "figgy pudding", "vanilla gelato", "banana bread", "apple strudel", "sponge cake", "rice pudding"];
 
-const activities = ["hula dancing", "golf", "tap dancing", "cricket", "badminton", "archery", "muay thai", "arm wrestling", "wrestling", "power lifting", "hockey", "step aerobics", "kung fu", "boxing", "water skiing", "basketball", "soccer", "racquetball", "swimming", "football", "bodybuilding", "rugby", "hot yoga", "ice skating"];
+const activities = ["hula dancing", "golf", "tap dancing", "cricket", "badminton", "archery", "muay thai", "arm wrestling", "wrestling", "power lifting", "hockey", "step aerobics", "kung fu", "boxing", "water skiing", "basketball", "soccer", "racquetball", "swimming", "football", "bodybuilding", "rugby", "hot yoga", "ice skating", "curling"];
 
 let select = array => {
 let randNum=Math.floor(Math.random() * array.length);
@@ -18,10 +19,10 @@ let phraseSelect = Math.round(Math.random());
 if(phraseSelect === 0){
 let thing1=select(actions);
 let thing2=select(places);
-console.log(`Touchdown like ${thing1} in a${thing2}--we are parked and proud!`);
+$("#message").html(`Touchdown like ${thing1} in a${thing2}--we are parked and proud!`);
 }
 else{
   let thing1=select(desserts);
   let thing2=select(activities);
-console.log(`Trainwreck ${thing1} is reporting tango ${thing2} is go.`);
+$("#message").html(`Trainwreck ${thing1} is reporting tango ${thing2} is go.`);
 };
--- test.java
+++
@@ -1,7 +0,0 @@
-public class Touchdown {
-  public static void main(String[] args) {
-    String[] actions = {"eating spicy food", "flamenco dancing", "colecting stamps"};
-    String[] places = {"toothbrush", "glasses case", "pocketbook"};
-    System.out.println("Touchdown like " + actions[0] + " in a " + places[1] + "--we are parked and proud!");
-  }
-}
