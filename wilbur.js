bf4832eb394f3e2413f1ee977247efff47de8de3
JC <james.calingo@gmail.com>
2020-09-07 15:03:12 -0400

    collected arrays into an object

--- wilbur.js
+++ wilbur.js
@@ -1,16 +1,14 @@
 // This file contains everything pertaining to Wilbur. These arrays are the various diologue options he has
 
-const actions = ["eating spicy food", "flamenco dancing", "collecting stamps", "surfing the web", "fixing things", "window shopping", "woodworking", "collecting figurines", "reading fortunes", "making playlists", "watching movies", "learning languages", "taking long naps", "watching trash TV", "practicing kung fu", "growing vegetables", "eating free pizza", "making mosaics", "bungee jumping", "clipping coupons", "watching trains", "scrapbooking", "doing brainteasers", "building models", "learning new stuff", "reading novels", "doing magic tricks", "playing card games", "break dancing", "doing paper crafts", "boogie boarding", "playing the kazoo", "playing the drums", "playing lacrosse", "polishing rocks"];
+const dialogue={ actions: ["eating spicy food", "flamenco dancing", "collecting stamps", "surfing the web", "fixing things", "window shopping", "woodworking", "collecting figurines", "reading fortunes", "making playlists", "watching movies", "learning languages", "taking long naps", "watching trash TV", "practicing kung fu", "growing vegetables", "eating free pizza", "making mosaics", "bungee jumping", "clipping coupons", "watching trains", "scrapbooking", "doing brainteasers", "building models", "learning new stuff", "reading novels", "doing magic tricks", "playing card games", "break dancing", "doing paper crafts", "boogie boarding", "playing the kazoo", "playing the drums", "playing lacrosse", "polishing rocks"],
 
-const places = ["toothbrush", "glasses case", "pocketbook", "microphone", "photo album", "microscope", "hand mirror", "change jar", "fitted sheet", "leg warmer", "blue crayon", "calculator", "saltshaker", "electric razor", "jewelry box", "ballpoint pen", "inkjet printer", "jackhammer", 
-"beach towel", "pencil case", "pack of gum", "glockenspiel", "kaleidoscope", "rubber band", "water bottle", "compost bin", "cotton swab", "toaster oven", "wristwatch", "mountain bike", "blowtorch", "tambourine", "picnic basket", "stethoscope", "paperweight", "pocketknife", "screwdriver", "pomade tin", "camera case", "water wing", "baking sheet", "space heater", "mixing bowl", "potpourri jar", "walking stick", "snowmobile", "socket wrench", "handkerchief", "encyclopedia", "megaphone", "wallet chain", "wall calendar"];
+ places: ["toothbrush", "glasses case", "pocketbook", "microphone", "photo album", "microscope", "hand mirror", "change jar", "fitted sheet", "leg warmer", "blue crayon", "calculator", "saltshaker", "electric razor", "jewelry box", "ballpoint pen", "inkjet printer", "jackhammer", 
+"beach towel", "pencil case", "pack of gum", "glockenspiel", "kaleidoscope", "rubber band", "water bottle", "compost bin", "cotton swab", "toaster oven", "wristwatch", "mountain bike", "blowtorch", "tambourine", "picnic basket", "stethoscope", "paperweight", "pocketknife", "screwdriver", "pomade tin", "camera case", "water wing", "baking sheet", "space heater", "mixing bowl", "potpourri jar", "walking stick", "snowmobile", "socket wrench", "handkerchief", "encyclopedia", "megaphone", "wallet chain", "wall calendar"],sweets : ["almond jelly", "gingerbread", "shaved ice", "cotton candy", "apple cobbler", "fried dough", "Bavarian cream", "rainbow sherbet", "key-lime pie", "apple cake", "peach cobbler", "tarte tatin", "crème brûlée", "coffee cake", "pumpkin pie", "coconut cream pie", "peanut brittle", "cheesecake", "spice cake", "fried ice cream", "bread pudding", "figgy pudding", "vanilla gelato", "banana bread", "apple strudel", "sponge cake", "rice pudding", "raisin bread", "mint ice cream", "lime sherbet", "blueberry pie", "bûche de Noël", "lemon sorbet", "frozen parfait", "coconut cake", "crème caramel", "shortbread", "carrot cake",  "yogurt parfait", "fruit parfait", "moon cake", "french toast"],
 
-const sweets = ["almond jelly", "gingerbread", "shaved ice", "cotton candy", "apple cobbler", "fried dough", "Bavarian cream", "rainbow sherbet", "key-lime pie", "apple cake", "peach cobbler", "tarte tatin", "crème brûlée", "coffee cake", "pumpkin pie", "coconut cream pie", "peanut brittle", "cheesecake", "spice cake", "fried ice cream", "bread pudding", "figgy pudding", "vanilla gelato", "banana bread", "apple strudel", "sponge cake", "rice pudding", "raisin bread", "mint ice cream", "lime sherbet", "blueberry pie", "bûche de Noël", "lemon sorbet", "frozen parfait", "coconut cake", "crème caramel", "shortbread", "carrot cake",  "yogurt parfait", "fruit parfait", "moon cake", "french toast"];
+ activities:["hula dancing", "golf", "tap dancing", "cricket", "badminton", "archery", "muay thai", "arm wrestling", "wrestling", "power lifting", "hockey", "step aerobics", "kung fu", "boxing", "water skiing", "basketball", "soccer", "racquetball", "swimming", "football", "bodybuilding", "rugby", "hot yoga", "ice skating", "curling", "baseball", "canoeing", "karate", "bocce", "bowling", "volleyball", "surfing", "capoeira", "tennis", "roller skating", "skateboarding", "table tennis", "tae kwon do", "snooker"],
 
-const activities = ["hula dancing", "golf", "tap dancing", "cricket", "badminton", "archery", "muay thai", "arm wrestling", "wrestling", "power lifting", "hockey", "step aerobics", "kung fu", "boxing", "water skiing", "basketball", "soccer", "racquetball", "swimming", "football", "bodybuilding", "rugby", "hot yoga", "ice skating", "curling", "baseball", "canoeing", "karate", "bocce", "bowling", "volleyball", "surfing", "capoeira", "tennis", "roller skating", "skateboarding", "table tennis", "tae kwon do", "snooker"];
-
-const musicStyles = ["rock", "salsa", "synthpop", "blues", "punk", "hip-hop", "disco", "doo-wop", "J-pop", "tango", "electronica", "reggae", "samba", "bebop", "bluegrass", "dubstep", "rockabilly", "cabaret", "karaoke", "opera", "folk", "mariachi", "zydeco", "techno", "jazz", "power pop", "country", "R&B", "heavy metal", "surf rock", "soul", "new wave", "bossa nova", "flamenco", "fusion", "bhangra", "mambo", "dancehall", "big band"];
-
+ musicStyles: ["rock", "salsa", "synthpop", "blues", "punk", "hip-hop", "disco", "doo-wop", "J-pop", "tango", "electronica", "reggae", "samba", "bebop", "bluegrass", "dubstep", "rockabilly", "cabaret", "karaoke", "opera", "folk", "mariachi", "zydeco", "techno", "jazz", "power pop", "country", "R&B", "heavy metal", "surf rock", "soul", "new wave", "bossa nova", "flamenco", "fusion", "bhangra", "mambo", "dancehall", "big band"]
+}
 // This function selects a random value from the array
 
 let select = arr => {
@@ -23,8 +21,8 @@ return selection;
 
 $("#arrive").on("click", function(e){
   e.preventDefault()
-let thing1=select(actions);
-let thing2=select(places);
+let thing1=select(dialogue.actions);
+let thing2=select(dialogue.places);
 // Because this sentence in particular has an a/an situation, we need to check if the first letter of the place is a vowel
 let vowelCheck = str =>{
   let firstLetter = (str.substring(0,1))
@@ -42,13 +40,8 @@ $("#message").html(`Touchdown like ${thing1} in a ${thing2}--we are parked and p
 })
 $("#depart").on("click", function(e){
   e.preventDefault()
-  let thing1=select(sweets);
-  let thing2=select(activities);
+  let thing1=select(dialogue.sweets);
+  let thing2=select(dialogue.activities);
 $("#message").html(`Trainwreck ${thing1} is reporting tango ${thing2} is go.`);
 }
 )
-// {
-
-// };
-
-// console.log(`Bellbottom ${select(musicStyles)} this is stovetop ${select(places)}, do you require assistance? Over.`)
